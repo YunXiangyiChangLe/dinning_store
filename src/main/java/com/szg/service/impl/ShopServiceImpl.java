c05fe0bacf2374b1a69a52eb3df86fff75eda41f
shaozeguang <shaozeguang@tju.edu.cn>
2025-06-06 14:55:34 +0800

    逻辑过期解决缓存击穿

--- src/main/java/com/szg/service/impl/ShopServiceImpl.java
+++ src/main/java/com/szg/service/impl/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.szg.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.szg.dto.Result;
 import com.szg.entity.Shop;
@@ -9,12 +10,16 @@ import com.szg.mapper.ShopMapper;
 import com.szg.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.szg.utils.RedisConstants;
+import com.szg.utils.RedisData;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
@@ -29,7 +34,9 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 //        Shop shop=queryWithPassThrough(id);
 
 //        互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+//        Shop shop = queryWithMutex(id);
+        //逻辑过期解决缓存击穿问题
+        Shop shop = queryWithLogicalExpire(id);
         if (shop == null) {
             return Result.fail("店铺不存在！？");
         }
@@ -73,7 +80,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             //不存在缓存查数据库
             shop = getById(id);
             //模拟测试重建延迟
-            Thread.sleep(200);
+//            Thread.sleep(200);
             if (shop == null) {
                 //将空值写入redis，解卷缓存穿透
                 stringRedisTemplate.opsForValue().set(key + id, "",
@@ -88,6 +95,46 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             unLock(lockKey);
         }
 
+        return shop;
+    }
+
+    public static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    public Shop queryWithLogicalExpire(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY;
+        //redis查询缓存
+        // redis未命中
+        String shopJson = stringRedisTemplate.opsForValue().get(key + id);
+        if (StrUtil.isBlank(shopJson)) {
+            return null;
+        }
+        //redis命中，查看是否过期
+        RedisData redisData = JSONUtil.toBean(shopJson, RedisData.class);
+        JSONObject data = (JSONObject) redisData.getData();
+        Shop shop = JSONUtil.toBean(data, Shop.class);
+
+        //未过期，返回
+        LocalDateTime expireTime = redisData.getExpireTime();
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            return shop;
+        }
+        //已过期，缓存重建
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+        //获取锁成功
+        if (isLock) {
+            //开启独立线程重建数据
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    saveShop2Redis(id, 20L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unLock(lockKey);
+                }
+            });
+        }
+        //获取锁失败，返货旧的店铺信息
         return shop;
     }
 
@@ -116,6 +163,18 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                 RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         return shop;
+    }
+
+    public void saveShop2Redis(Long id, Long expireSeconds) throws InterruptedException {
+        Shop shop = getById(id);
+//        Thread.sleep(200);
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireSeconds));
+
+        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id,
+                JSONUtil.toJsonStr(redisData));
+
     }
 
     @Override
--- src/test/java/com/szg/HeimaDianpingApplicationTests.java
+++ src/test/java/com/szg/HeimaDianpingApplicationTests.java
@@ -1,11 +1,20 @@
 package com.szg;
 
+import com.szg.service.impl.ShopServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class HeimaDianpingApplicationTests {
+    @Resource
+    private ShopServiceImpl shopService;
 
+    @Test
+    void testSaveDate() throws InterruptedException {
+        shopService.saveShop2Redis(1L,10L);
+    }
     @Test
     void contextLoads() {
     }
